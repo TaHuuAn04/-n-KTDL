e225b970c9e2f7042bbd633dd45edfd3de741b07
dung <dungduhau@gmail.com>
2024-11-27 17:40:31 +0700

    add api of customer

--- fin-main/Backend/index.js
+++ fin-main/Backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // Import route
 const loginRoute = require('./login');
 const productRoutes = require('./products')
+const customerRoutes = require('./customers')
 // Kết nối MongoDB
 mongoose.connect(
     "mongodb+srv://antaduychinh:abcxyz04@fashionshop.qdyue.mongodb.net/fashion?retryWrites=true&w=majority&appName=Fashionshop"
@@ -17,6 +18,7 @@ mongoose.connect(
 // Routes
 app.use('/api', loginRoute);
 app.use('/products', productRoutes);
+app.use('/customers', customerRoutes);
 
 app.listen(3000, () => {
     console.log("Connect successfully!");
